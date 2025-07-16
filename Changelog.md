39b2b65fb20bfe4ff3b897c487e122f4f952a940
Roberto341 <will69558@gmail.com>
2025-07-16 02:55:16 -0700

    Added Update MapEditor v-a.0.2

--- Changelog.md
+++ Changelog.md
@@ -5,3 +5,27 @@
 - July 11th 2025 - Added ImGui
 - July 12th 2025 - Implmented ImGui - Sorta- and added a Map Editor
 - Starting on July 12th I will be over hauling the map editor and relasing a full application for making maps and what not that will be available to the public
+- Update July 13th todays commit has been pushed back to tomorrow because the map editor experienced a major bug dont worry I fixed it and the new version will be released in the morning 
+ I also went and overhauled the map editor significantly it now features 
+   - Configuration file loading to load assests in 
+   - Textured tile display and normal Vec4 display
+   - Fixed a issue with the imgui grid not matching the main window 
+   - small tweaks to the save and load .world files 
+- Also I've been contemplating this one so I'll break it down for ya 
+- The game engine doesn't have a world loader its only in the game I'm currently making however I can implement it into the engine with a few things 
+  - 1 only .world files will be supported 
+  - 2 you wont be able to render any npcs 
+  - multiple sprites wont render either 
+  - Just so you know I'm working on a fix for it but it might take awhile I'm a one man band right now so please be patient 
+- Update July 16th 2025
+    - MapEditor class now has the ability to add textures and color tiles you can also make new ones directly in the editor (textures must follow below guidelines)
+        - Textures must be 24 bit to read otherwise it wont work
+        - Textures can be any file type .png, .jpg, .bmp, .dds
+        - As long as it's saved as 24 bit it will work
+        - I recomend using [Paint.net](https://www.getpaint.net/download.html) 
+    - Also MapEditor now has the ability to add spawn points for players, npcs, and enemey
+    - I've started working on adding Objects as well
+    - At a latter date I will add collision directly in the map editor 
+    - Other features will be added as well as time goes by
+    - Current projection for the MapEditor release will be later in the begining of 2026 or mid 2026 
+    - Other news I will be rebuilding FayEngine as a library it will have 2D and 3D support as well as the 3D Map Editor which will come at a latter date 
--- Fay/main.cpp
+++ Fay/main.cpp
@@ -1,7 +1,62 @@
+#define MAKING_GAME 0
+#define MAP_EDITOR 1
+#if MAKING_GAME
+#include <Game.h>
+#include <Player.h>
+#include <ctime>
+#else
 #include <Core/Core.h>
-#define MAP_EDITOR 0
+#endif
 int main()
 {
+#if MAKING_GAME
+	srand(static_cast<unsigned>(time(0)));
+	Game::Game game("In the dark", 920, 540);
+	// game
+	enum GameStates
+	{
+		STATE_MENU,
+		STATE_PLAYING,
+		STATE_PAUSED
+	};
+	//game.getGroup()->add(player.getDetailsLabel());
+	//group.add(game.getFpsLabel());
+
+	//game.getGroup()->add(game.getGroupBack());
+	game.getTileLayer()->add(game.getGroup()); //fps
+	game.getTileLayer()->add(game.getGroup2()); //player hud
+	//game.getTileLayer2()->add(player.getSprite()); // player sprite
+
+	// update window
+	auto win = game.getWindow();
+	while (!win->closed())
+	{
+		//win->bindRenderTarget(); // uncomment when using viewport
+		win->clear();
+		game.getTileLayer()->render();
+		game.getTileLayer2()->render();
+
+		if (win->isKeyPressed(GLFW_KEY_ESCAPE))
+			if (game.gameState.state == 1)
+				game.setGameState(2);
+			else if(game.gameState.state == 2)
+				game.setGameState(1);
+		switch (game.gameState.state) {
+		case STATE_MENU:
+			break;
+		case STATE_PLAYING:
+			game.update();
+		case STATE_PAUSED:
+			break;
+		}
+		//win->unbindRenderTarget(); // uncomment when using viewport
+		win->update();
+	}
+
+	// clean method
+	//player.clean();
+	game.clean();
+#else
 #if !MAP_EDITOR
 	Fay::Window window("RobCo", 920, 540);
 	Fay::Camera camera(920, 540);
@@ -73,23 +128,41 @@ Fay::Shader* s = new Fay::Shader("Res/Shaders/basic.vert", "Res/Shaders/basic.fr
 Fay::Shader& shader = *s;
 Fay::TileLayer* layer = new Fay::TileLayer(&shader);
 
-Fay::MapEditor editor(layer, 32, 18, 32.0f);
+Fay::Shader* s2 = new Fay::Shader("Res/Shaders/basic.vert", "Res/Shaders/basic.frag");
+Fay::Shader& shader2 = *s2;
+Fay::TileLayer* layer2 = new Fay::TileLayer(&shader2);
+
+//Fay::TileLayer& layer = *lay;
+
+Fay::MapEditor editor(layer, layer2, 32, 18, 32.0f);
 Fay::Camera camera(920, 540);
+Fay::Camera camera2(920, 540);
+
 
 
 shader.enable();
 shader.setUniform2f("light_pos", Fay::Vec2(4.0f, 1.5f)); // lighting
 
+shader2.enable();
+shader2.setUniform2f("light_pos", Fay::Vec2(4.0f, 1.5f)); // lighting
+
 GLint texIds[]
 {
-	0, 1, 2, 3, 4, 5, 6, 7, 9, 9
+	0, 1, 2, 3, 4, 5, 6, 7, 8, 9
 };
 shader.enable();
 shader.setUniform1iv("textures", texIds, 10);
 shader.setUniformMat4("pr_matrix", Fay::Mat4::orthographic(-16.0f, 16.0f, -9.0f, 9.0f, -1.0f, 1.0f)); // change to use camera->getProjection();
 shader.setUniformMat4("vw_matrix", camera.getViewMatrix());
-Fay::Vec3 camPos;
 
+shader2.enable();
+shader2.setUniform1iv("textures", texIds, 10);
+shader2.setUniformMat4("pr_matrix", Fay::Mat4::orthographic(-16.0f, 16.0f, -9.0f, 9.0f, -1.0f, 1.0f)); // change to use camera->getProjection();
+shader2.setUniformMat4("vw_matrix", camera2.getViewMatrix());
+Fay::Vec3 camPos;
+//ImGui_ImplOpenGL3_NewFrame();
+//ImGui_ImplGlfw_NewFrame();
+//ImGui::NewFrame();
 IMGUI_CHECKVERSION();
 ImGui::CreateContext();
 ImGuiIO& io = ImGui::GetIO();
@@ -109,6 +182,15 @@ ImGui_ImplOpenGL3_Init("#version 330");
 
 while (!window.closed())
 {
+
+	camera.setPosition(Fay::Vec3(16.0f, 9.0f, 0.0f));
+
+	shader.enable();
+	shader.setUniformMat4("vw_matrix", camera.getViewMatrix());
+
+	shader2.enable();
+	shader2.setUniformMat4("vw_matrix", camera.getViewMatrix());
+
 	window.clear();
 	ImGui_ImplOpenGL3_NewFrame();
 	ImGui_ImplGlfw_NewFrame();
@@ -118,6 +200,7 @@ while (!window.closed())
 	editor.handleInput();
 	editor.render();
 	layer->render();
+	layer2->render();
 	editor.update();
 
 	//game.getTileLayer()->render();
@@ -133,11 +216,26 @@ while (!window.closed())
 	}
 	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-	camera.setPosition(Fay::Vec3(16.0f, 9.0f, 0.0f));
-	shader.setUniformMat4("vw_matrix", camera.getViewMatrix());
 
+	/*
+	if (window.isKeyPressed(GLFW_KEY_A))
+		camPos.x -= 0.1f;
+	else if (window.isKeyPressed(GLFW_KEY_D))
+		camPos.x += 0.1f;
+	if (window.isKeyPressed(GLFW_KEY_W))
+		camPos.y += 0.1f;
+	else if (window.isKeyPressed(GLFW_KEY_S))
+		camPos.y -= 0.1f;
+	*/
+
+	//camera.setPosition(Fay::Vec3(camPos.x, camPos.y, 0));
+	//std::cout << camPos << std::endl;
 	window.update();
 }
+
+#endif
+
+
 #endif
 	return 0;
 }
